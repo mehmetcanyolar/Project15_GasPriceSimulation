2369325088bbbf111470a33af7564aa90ae99491
mehmet can yolar <mehmetcanyolar@gmail.com>
2024-12-06 00:26:17 +0300

    06/12/24

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,6 +38,19 @@
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.btnStart = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtGasolinePrice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDieselPrice = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLpgPrice = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rdbGas = new System.Windows.Forms.RadioButton();
+            this.rdbDiesel = new System.Windows.Forms.RadioButton();
+            this.rdbLpg = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -54,7 +67,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(60, 129);
+            this.label2.Location = new System.Drawing.Point(54, 296);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(114, 23);
             this.label2.TabIndex = 1;
@@ -63,7 +76,7 @@
             // txtGasAmount
             // 
             this.txtGasAmount.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
-            this.txtGasAmount.Location = new System.Drawing.Point(172, 129);
+            this.txtGasAmount.Location = new System.Drawing.Point(180, 296);
             this.txtGasAmount.Name = "txtGasAmount";
             this.txtGasAmount.Size = new System.Drawing.Size(260, 30);
             this.txtGasAmount.TabIndex = 2;
@@ -71,7 +84,7 @@
             // txtTotalPrice
             // 
             this.txtTotalPrice.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
-            this.txtTotalPrice.Location = new System.Drawing.Point(172, 175);
+            this.txtTotalPrice.Location = new System.Drawing.Point(180, 342);
             this.txtTotalPrice.Name = "txtTotalPrice";
             this.txtTotalPrice.Size = new System.Drawing.Size(260, 30);
             this.txtTotalPrice.TabIndex = 4;
@@ -80,7 +93,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(60, 175);
+            this.label3.Location = new System.Drawing.Point(54, 342);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(114, 23);
             this.label3.TabIndex = 3;
@@ -90,7 +103,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(51, 228);
+            this.label4.Location = new System.Drawing.Point(45, 395);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(123, 23);
             this.label4.TabIndex = 5;
@@ -98,9 +111,9 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(172, 225);
+            this.progressBar1.Location = new System.Drawing.Point(174, 395);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(536, 31);
+            this.progressBar1.Size = new System.Drawing.Size(731, 31);
             this.progressBar1.TabIndex = 6;
             // 
             // btnStart
@@ -108,19 +121,143 @@
             this.btnStart.BackColor = System.Drawing.Color.Linen;
             this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnStart.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStart.Location = new System.Drawing.Point(298, 312);
+            this.btnStart.Location = new System.Drawing.Point(306, 479);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(165, 36);
+            this.btnStart.Size = new System.Drawing.Size(190, 36);
             this.btnStart.TabIndex = 7;
             this.btnStart.Text = "İŞLEMİ BAŞLAT";
             this.btnStart.UseVisualStyleBackColor = false;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLpgPrice);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtDieselPrice);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtGasolinePrice);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(600, 129);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(365, 243);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fiyatlar";
+            // 
+            // txtGasolinePrice
+            // 
+            this.txtGasolinePrice.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            this.txtGasolinePrice.Location = new System.Drawing.Point(165, 38);
+            this.txtGasolinePrice.Name = "txtGasolinePrice";
+            this.txtGasolinePrice.ReadOnly = true;
+            this.txtGasolinePrice.Size = new System.Drawing.Size(113, 30);
+            this.txtGasolinePrice.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(79, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 23);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Benzin: ";
+            // 
+            // txtDieselPrice
+            // 
+            this.txtDieselPrice.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            this.txtDieselPrice.Location = new System.Drawing.Point(168, 91);
+            this.txtDieselPrice.Name = "txtDieselPrice";
+            this.txtDieselPrice.ReadOnly = true;
+            this.txtDieselPrice.Size = new System.Drawing.Size(113, 30);
+            this.txtDieselPrice.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(93, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 23);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Dizel: ";
+            // 
+            // txtLpgPrice
+            // 
+            this.txtLpgPrice.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            this.txtLpgPrice.Location = new System.Drawing.Point(168, 144);
+            this.txtLpgPrice.Name = "txtLpgPrice";
+            this.txtLpgPrice.ReadOnly = true;
+            this.txtLpgPrice.Size = new System.Drawing.Size(113, 30);
+            this.txtLpgPrice.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(102, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 23);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Lpg: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdbLpg);
+            this.groupBox2.Controls.Add(this.rdbDiesel);
+            this.groupBox2.Controls.Add(this.rdbGas);
+            this.groupBox2.Location = new System.Drawing.Point(123, 133);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(396, 60);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Yakıt Türü";
+            // 
+            // rdbGas
+            // 
+            this.rdbGas.AutoSize = true;
+            this.rdbGas.Location = new System.Drawing.Point(57, 22);
+            this.rdbGas.Name = "rdbGas";
+            this.rdbGas.Size = new System.Drawing.Size(68, 20);
+            this.rdbGas.TabIndex = 0;
+            this.rdbGas.TabStop = true;
+            this.rdbGas.Text = "Benzin";
+            this.rdbGas.UseVisualStyleBackColor = true;
+            // 
+            // rdbDiesel
+            // 
+            this.rdbDiesel.AutoSize = true;
+            this.rdbDiesel.Location = new System.Drawing.Point(154, 21);
+            this.rdbDiesel.Name = "rdbDiesel";
+            this.rdbDiesel.Size = new System.Drawing.Size(67, 20);
+            this.rdbDiesel.TabIndex = 1;
+            this.rdbDiesel.TabStop = true;
+            this.rdbDiesel.Text = "Diesel";
+            this.rdbDiesel.UseVisualStyleBackColor = true;
+            // 
+            // rdbLpg
+            // 
+            this.rdbLpg.AutoSize = true;
+            this.rdbLpg.Location = new System.Drawing.Point(262, 20);
+            this.rdbLpg.Name = "rdbLpg";
+            this.rdbLpg.Size = new System.Drawing.Size(51, 20);
+            this.rdbLpg.TabIndex = 2;
+            this.rdbLpg.TabStop = true;
+            this.rdbLpg.Text = "Lpg";
+            this.rdbLpg.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1065, 545);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label4);
@@ -130,6 +267,11 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -146,6 +288,17 @@
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtLpgPrice;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDieselPrice;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtGasolinePrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdbLpg;
+        private System.Windows.Forms.RadioButton rdbDiesel;
+        private System.Windows.Forms.RadioButton rdbGas;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,10 +19,151 @@ namespace Project15_GasPriceSimulation
         {
             InitializeComponent();
         }
+        double totalPrice;
+        double dieselPrice = 0;
+        double gasolinePrice =0;
+        double lpgPrice = 0;
+        double gasAmount;
+        double dieselAmount;
+        double lpgAmount;
+        int count;
+
+      
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            gasAmount = Convert.ToDouble(txtGasAmount.Text);
+            lpgAmount = Convert.ToDouble(txtGasAmount.Text);
+            dieselAmount = Convert.ToDouble(txtGasAmount.Text);
+           
             timer1.Start();
+            timer1.Interval = 80;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = count.ToString();
+
+
+            if (rdbGas.Checked)
+            {
+                count++;
+                if (count <= gasAmount)
+                {
+
+                    totalPrice += gasolinePrice;
+                    txtTotalPrice.Text = totalPrice.ToString() + " ₺";
+                }
+                else
+                {
+                    txtTotalPrice.Text = totalPrice.ToString() + " ₺";
+                }
+                progressBar1.Value += 1;
+
+                if (progressBar1.Value == 99)
+                {
+                    timer1.Stop();
+
+                }
+            }
+            if (rdbDiesel.Checked)
+            {
+                count++;
+                if (count <= dieselAmount)
+                {
+
+                    totalPrice += dieselPrice;
+                    txtTotalPrice.Text = totalPrice.ToString() + " ₺";
+                }
+                else
+                {
+                    txtTotalPrice.Text = totalPrice.ToString() + " ₺";
+                }
+                progressBar1.Value += 1;
+
+                if (progressBar1.Value == 99)
+                {
+                    timer1.Stop();
+
+                }
+            }
+
+            if (rdbLpg.Checked)
+            {
+                count++;
+                if (count <= lpgAmount)
+                {
+
+                    totalPrice += lpgPrice;
+                    txtTotalPrice.Text = totalPrice.ToString() + " ₺";
+                }
+                else
+                {
+                    txtTotalPrice.Text = totalPrice.ToString() + " ₺";
+                }
+
+                progressBar1.Value += 3;
+
+                if (progressBar1.Value == 99)
+                {
+                    timer1.Stop();
+
+                }
+            }
+            
+
+            
+        }
+
+        public async void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("API YAKIT  VERİLERİ ALINIYOR...");
+
+
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://akaryakit-fiyatlari.p.rapidapi.com/fuel/istanbul"),
+                Headers =
+    {
+        { "x-rapidapi-key", "8e2153b60cmshad87d4c02fb4d8fp1ddcabjsned53ab11d2ad" },
+        { "x-rapidapi-host", "akaryakit-fiyatlari.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var json = JObject.Parse(body);
+                //var gasolineJsonValue = json["data"][16]["prices"][0] ["benzin"];
+                //var dieselJsonValue = json["data"][16]["prices"][0] ["motorin"];
+                //var lpgJsonValue = json["data"][16]["prices"][0] ["lpg"];
+
+                // "₺" simgesini kaldırıp, "," ondalık ayracını "." ile değiştirelim
+                var gasolineJsonValue = json["data"][17]["prices"][0]["benzin"].ToString().Replace("₺", "").Replace(",", ".");
+                gasolinePrice = double.Parse(gasolineJsonValue, CultureInfo.InvariantCulture);
+
+                var dieselJsonValue = json["data"][17]["prices"][0]["motorin"].ToString().Replace("₺", "").Replace(",", ".");
+                dieselPrice = double.Parse(dieselJsonValue, CultureInfo.InvariantCulture);
+
+                var lpgJsonValue = json["data"][17]["prices"][0]["lpg"].ToString().Replace("₺", "").Replace(",", ".");
+                lpgPrice = double.Parse(lpgJsonValue, CultureInfo.InvariantCulture);
+
+
+                txtGasolinePrice.Text = gasolineJsonValue.ToString() + " ₺";
+                txtDieselPrice.Text = dieselJsonValue.ToString() + " ₺";
+                txtLpgPrice.Text = lpgJsonValue.ToString() + " ₺";
+
+                gasolinePrice = double.Parse(gasolineJsonValue.ToString());
+                dieselPrice = double.Parse(dieselJsonValue.ToString());
+                lpgPrice = double.Parse(lpgJsonValue.ToString());
+
+            }
+
+
+
         }
     }
 }
